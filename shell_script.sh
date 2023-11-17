fc83144785171c43073e2f15de1ee47ca0c1fa6f
Gagan Chaurasiya <gagan.chaurasiya@lseg.com>
2023-11-17 22:47:51 +0530

    Change

--- shell_script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Hello World"
-
-for file in /mnt/g/Shekhar/SDIFileDownloadUpload/SDIFile** ; do 
-    dirName=$(awk -F[-.] '{print $3}' <<<"$file"); 
-    echo mv "$file" "/mnt/g/Shekhar/SDIFileDownloadUpload/SDIFile/$dirName"; 
-done
